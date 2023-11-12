cb55b82c354698ee47e60dbc28a7117eabde26f5
Mustafa Ekşi <mustafa.eskieksi@gmail.com>
2023-11-12 10:02:37 +0300

    Fixed crash from trying to registor with an invalid password.

--- Api/Controllers/Auth.ts
+++ Api/Controllers/Auth.ts
@@ -1,14 +1,12 @@
 import { LogicRegister } from "../../Logic";
 
 export async function AuthRegister(req: any, res: any, next: any) {
-    //console.log(req);
-    let result = await LogicRegister(req.body);
-    if(typeof result == "boolean") {
+    // FIXME: replace this with a beautiful await syntax
+    let result = await LogicRegister(req.body).then((result)=>{
         return res.send({res: result})
-    } else {
-        res.send({res: false, error: result.message});
-    }
-    
+    }).catch((err)=>{
+        res.send({res: false, error: err});
+    });
 }
 
 export async function AuthCheckPermission(req: any, res:any, next: any) {
